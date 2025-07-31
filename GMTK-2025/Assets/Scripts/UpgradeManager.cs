1cffe825c7ad7a99b961277b400154430bb3fefe
Ansel Stanik <doomkitty887@gmail.com>
2025-07-31 13:42:27 -0700

    add upgrade manager

---
+++ GMTK-2025/Assets/Scripts/UpgradeManager.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+public class UpgradeManager : MonoBehaviour
+{
+    public static UpgradeManager Instance { get; private set; }
+
+    [System.Serializable]
+    private class Upgrade
+    {
+
+        public string Name;
+        public GameObject[] EnabledObjects;
+        public GameObject[] DisabledObjects;
+        public bool IsOwned;
+
+    }
+
+    [SerializeField] private Upgrade[] upgrades;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        foreach (var upgrade in upgrades)
+        {
+            UpdateUpgradeState(upgrade);
+        }
+    }
+
+    private void UpdateUpgradeState(Upgrade upgrade)
+    {
+        if (upgrade.IsOwned)
+        {
+            foreach (var obj in upgrade.EnabledObjects)
+            {
+                obj.SetActive(true);
+            }
+            foreach (var obj in upgrade.DisabledObjects)
+            {
+                obj.SetActive(false);
+            }
+        }
+        else
+        {
+            foreach (var obj in upgrade.EnabledObjects)
+            {
+                obj.SetActive(false);
+            }
+            foreach (var obj in upgrade.DisabledObjects)
+            {
+                obj.SetActive(true);
+            }
+        }
+    }
+    
+}
