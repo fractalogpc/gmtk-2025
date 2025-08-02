eece8bb1792a3daf379f20be6fca226830eee6b8
Matthew Dinh <matthewhdinhhmk@gmail.com>
2025-08-02 15:22:59 -0700

    added ui

--- GMTK-2025/Assets/Scripts/DisplayGameState.cs
+++ GMTK-2025/Assets/Scripts/DisplayGameState.cs
@@ -7,14 +7,28 @@ public class DisplayGameState : MonoBehaviour
 	[SerializeField] private TextMeshProUGUI countdownText;
 	[SerializeField] private TextMeshProUGUI dayText;
 	[SerializeField] private TextMeshProUGUI quotaText;
+	[SerializeField] private ClockManager clockManager;
 
-	private void Start() {
+	private void Start()
+	{
 		gameManager = GameManager.Instance;
 	}
 
-	private void Update() {
-		countdownText.text = TimeSpan.FromSeconds(gameManager.timeLeftInDay).ToString(@"mm\:ss");
-		dayText.text = $"Day {gameManager.currentDay.ToString()}";
-		quotaText.text = $"Need {gameManager.sheepQuota.ToString()} sheep";
+	private void Update()
+	{
+		if (gameManager.gameState == GameManager.GameState.CollectSheep)
+		{
+			clockManager.UpdateClock(gameManager.timeLeftInDay, gameManager.dayLengthMinutes * 60, false);
+			countdownText.text = TimeSpan.FromSeconds(gameManager.timeLeftInDay).ToString(@"m\:ss");
+			dayText.text = $"Day {gameManager.currentDay.ToString()}";
+			quotaText.text = $"{gameManager.sheepQuota.ToString()} sheep";
+		}
+		else
+		{
+			clockManager.UpdateClock(gameManager.timeLeftInDay, gameManager.dayLengthMinutes, true);
+			countdownText.text = "";
+			dayText.text = $"Day {gameManager.currentDay.ToString()}";
+			quotaText.text = $"{gameManager.numSheepOffered.ToString()} / {gameManager.sheepQuota.ToString()} sheep";
+		}
 	}
 }
